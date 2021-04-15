7d24fb6e40a34910d609afe5e9644809b152b621
DewhiteE <dewhiteeofficial@gmail.com>
2021-04-15 13:35:18 +0300

    update lab1

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def start_simulation(overall_simulation_time=500):
 
     def print_departure_time():
         nonlocal departure_time
-        return f'{round(departure_time, 2):<12}'
+        return f'{round(departure_time, 2):<17}'
 
     def arrive():
         nonlocal delta_time, queue, departure_time, arrival_time
@@ -76,12 +76,13 @@ def start_simulation(overall_simulation_time=500):
         return current_min_index
 
     def depart():
-        nonlocal delta_time, queue, departure_time
+        nonlocal delta_time, queue, departure_time, idle_start
 
         delta_time = departure_time
         current_class = queue.pop(0)
         if len(queue) == 0:
             departure_time = float('inf')
+            idle_start = delta_time
         else:
             departure_time = delta_time + departure[current_class]()
         print(f'{"(DEPARTURE)":<12} ', f'{("(" + str(current_class) + ")"):<6}', print_delta_time(),
@@ -104,9 +105,10 @@ def start_simulation(overall_simulation_time=500):
 
         # Check if idle
         if len(queue) == 0 and idle_start != 0:
-            idle_finish = delta_time
+            #idle_finish = delta_time
+            idle_finish = min(arrival_time)
             idle_total += idle_finish - idle_start
-            print(f'{"(IDLE)":<18} ', print_delta_time(), f': {0:<12}')
+            print(f'{"(IDLE)":<19} ', print_delta_time(), f': {0:<12}')
 
         total_event_count += 1
 
@@ -114,58 +116,10 @@ def start_simulation(overall_simulation_time=500):
           f'queue: {queue}, status: {"empty" if len(queue) == 0 else "busy"}, '
           f'overall_simulation_time: {overall_simulation_time}, time: {arrival_time}'
           f'\n-------------------------------------------------------------\n'
-          f'{"Event":<10} | {"Class"} | {"Delta time"} | {"Items in system"} | {"Departure at"} | q')
+          f'{"Event":<10} | {"Class"} | {"Delta time"} | {"Items in system"} | {"Next departure at"} | q')
 
     while delta_time < overall_simulation_time:
         advance_in_time()
-
-        # Updating the total (delta) time
-        # delta_time = current_min_time = min(arrival_time)
-
-        # If departure time is the nearest time - freeing the server and removing current active event from the queue
-        # Event 3
-        # if departure_time < current_min_time:
-        # Remove the current active event from the queue
-        # queue = queue[1:]
-        # queue.pop(0)
-
-        # Free the server
-        # status = 0
-
-        # Check if idle
-        # if len(queue) == 0 and idle_start != 0:
-        #    idle_finish = delta_time
-        #    idle_total += idle_finish - idle_start
-        #     continue
-
-        # Get index of a nearest event time (type 1 or type 2)
-        # current_min_index = arrival_time.index(current_min_time)
-
-        # Updating nearest event's time
-        # arrival_time[current_min_index] += current_min_time
-
-        # If server is Free - calculate and update the departure time
-        # if status == 0:
-        # if len(queue) == 0:
-        # Updating departure time to the time of a currently nearest event
-        #    departure_time = arrival_time[current_min_index] + departure[current_min_index]()
-
-        # Set server status to busy
-        # status = 1
-
-        # Add current event to the queue (type 1 or type 2)
-        # queue.append(current_min_index + 1)
-
-        # Update total count of events
-        # total_event_count += 1
-
-        # print(f'delta_time: {delta_time}, '
-        #      f'total_event_count: {total_event_count}, '
-        #      f'current_min_index: {current_min_index}, current_min_time: {current_min_time}\n'
-        #      f'time: {arrival_time}\n'
-        #      f'departure_time: {departure_time}\n'
-        #      f'queue: {queue}\n'
-        #      f'idle_total: {idle_total}, idle_finish: {idle_finish}, idle_start: {idle_start}\n')
 
     # Calculate idle coefficient
     idle_coefficient = idle_total / overall_simulation_time
